6171ccda231ff097958a1ba60836e549605c0bb9
Morgan <heinemann.morgan@gmail.com>
2020-07-13 12:45:17 -0700

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# cypher-character-generator-frontend
-React frontend for the Cypher System character creator
-
-## Concept
-This is a personal project to use Redact and Redux to make a character builder for the Cypher System, a tabletop RPG.
-Current state as of 7/9/2020 -> basic character options and stats are in a rough 'layout' and are being rigged up to the store and other logic.
-Backend posses some of the required routes for GET requests but there is no current implementation to save a character for later (a to-do item)
---
+++ precache-manifest.01cc7ee9286d22a6b2450b0bf33b5bb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a2c0e58e7bbcb1f4a2c0b0447486a5",
+    "url": "/cypher-character-generator-frontend/index.html"
+  },
+  {
+    "revision": "af81996853e62b7022b9",
+    "url": "/cypher-character-generator-frontend/static/css/main.63332aba.chunk.css"
+  },
+  {
+    "revision": "66e21f58a3ace81f6a63",
+    "url": "/cypher-character-generator-frontend/static/js/2.5c1c0454.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cypher-character-generator-frontend/static/js/2.5c1c0454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af81996853e62b7022b9",
+    "url": "/cypher-character-generator-frontend/static/js/main.0298b334.chunk.js"
+  },
+  {
+    "revision": "7e6e6365ee37ec86a5d4",
+    "url": "/cypher-character-generator-frontend/static/js/runtime-main.1487ae3d.js"
+  },
+  {
+    "revision": "9bab419b35dee7c2a678559592dca6a3",
+    "url": "/cypher-character-generator-frontend/static/media/charSheet.9bab419b.JPG"
+  }
+]);
